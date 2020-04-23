846e6f46f51ce98fe5ce157046f4a1881d76713f
Zinaida <zinaida.ivanova.1988@mail.ru>
2020-04-23 10:57:21 +0300

    изменение индикатора слайдеров

--- js/main.js
+++ js/main.js
@@ -5,25 +5,37 @@ let portfolioBtnOpen = document.querySelector('.link__portfolio_open');
 let portfolioMenu = document.querySelector('.menu-portfolio__list');
 let serchForm = document.querySelector('.form-search');
 let accordionContainer = document.querySelectorAll('.acordion__item_big');
-let accordionTitle = document.querySelectorAll('.accorfion__link');
+let accordionContainerSmall = document.querySelectorAll('.acordion__item_small');
+let accordionTitle = document.querySelectorAll('.accorfion__link_big');
+let accordionTitleSmall = document.querySelectorAll('.accorfion__link_small');
+let classActive = 'active-tabs';
+let menuActive = 'active-menu';
+let classMenuActive = 'active';
 
-btnOpenMenu.addEventListener('click', function(e){
-    e.preventDefault();
-    menu.classList.toggle('active');
-});
 
-serchBtnOpen.addEventListener('click', function(e){
-    e.preventDefault();
-    serchForm.classList.toggle('active-menu');
-});
-portfolioBtnOpen.addEventListener('click', function(e){
-    e.preventDefault();
-    portfolioMenu.classList.toggle('active-menu');
-});
+openMenu(btnOpenMenu, menu, classMenuActive);
+openMenu(serchBtnOpen, serchForm, menuActive)
+openMenu(portfolioBtnOpen, portfolioMenu, menuActive)
 
-accordionTitle.forEach(function(item, accordionTitle){
+openAccordion(accordionTitle, accordionContainer, classActive);
+openAccordion(accordionTitleSmall, accordionContainerSmall , classActive);
+
+
+function openMenu(btn, hidenMenu, activClass){
+    btn.addEventListener('click', function(e){
+    e.preventDefault();
+    hidenMenu.classList.toggle(activClass);
+    });
+};
+
+function openAccordion(titles, body, classActive){
+    titles.forEach(function(item, accordionTitle){
     item.addEventListener('click', function(e) {
-    e.preventDefault();
-        e.target.parentNode.classList.toggle('active-tabs');
+    body.forEach(function(item){
+        item.classList.remove(classActive);
     })
-});
+    e.preventDefault();
+        e.target.parentNode.classList.toggle(classActive);
+    });
+    });
+}
